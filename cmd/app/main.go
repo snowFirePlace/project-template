cb23311e377feb2f60a015ddb688e00ad54c44c3
snowFirePlace <snowFireplace@outlook.com>
2024-02-13 09:25:04 +0300

    new file:   README.md 	new file:   api/readme.md 	new file:   build/Dockerfile 	new file:   cmd/app/main.go 	new file:   configs/main.yaml 	new file:   deployments/readme.md 	new file:   docs/readme.md 	new file:   go.mod 	new file:   internal/app/app.go 	new file:   internal/config/config.go 	new file:   internal/models/model.go 	new file:   internal/services/service.go 	new file:   internal/transport/rest/handler.go 	new file:   pkg/public-lib/readme.md

---
+++ cmd/app/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/snowFirePlace/project-template/internal/app"
+)
+
+func main() {
+	app.Run()
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/snowFirePlace/project-template
+
+go 1.19
